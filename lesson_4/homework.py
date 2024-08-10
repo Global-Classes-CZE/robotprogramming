fba42f4e0b09107ae6f703155031cd92fff4ea30
Lenka Mudrich <lenka.mudrich@gmail.com>
2024-08-10 14:58:29 +0100

    Pridana definice funkci

--- lesson_4/homework.py
+++ lesson_4/homework.py
@@ -2,7 +2,17 @@ from neopixel import NeoPixel
 from microbit import pin0
 from microbit import sleep
 
+zapni(poradi_led):
+    #TODO
+
+vypni(poradi_led):
+    #TODO
+
+nastav_barvu(poradi_led, barva):
+    #TODO
+
 np = NeoPixel(pin0,8)
+#TODO: predelejte smycku tka, aby volala vyse definovane funkce
 while True:
     np[0] = (255,255,255) # nastavim prvni ledku, tzn [0] na bilou (RGB hodnoty)
     np.write()
