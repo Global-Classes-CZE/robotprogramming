0e16d1dd3e7cc81c025425aaf189a7804dea594e
aihatn <150720958+aihatn@users.noreply.github.com>
2024-08-14 16:47:23 +0200

    Homework 4
    Kamil Chrastina

--- lesson_4/homework.py
+++ lesson_4/homework.py
@@ -3,21 +3,19 @@ from microbit import pin0
 from microbit import sleep
 
 def zapni(poradi_led):
-    #TODO
+    nastav_barvu (poradi_led, (255, 255, 255))
 
 def vypni(poradi_led):
-    #TODO
+    nastav_barvu (poradi_led, (0, 0, 0))
 
 def nastav_barvu(poradi_led, barva):
-    #TODO
+    np[poradi_led] = barva
+    np.write()
 
 np = NeoPixel(pin0,8)
 #TODO: predelejte smycku tak, aby volala vyse definovane funkce
 while True:
-    np[0] = (255,255,255) # nastavim prvni ledku, tzn [0] na bilou (RGB hodnoty)
-    np.write()
+    zapni(0)
     sleep(1000)
-    np[0] = (0,0,0) # nastavim prvni ledku, tzn [0] na cernou (RGB hodnoty)
-    np.write()
+    vypni(0)
     sleep(1000)
-
