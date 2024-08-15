8dc069ea701ed6e588d5219522cb8ab8eb4f32af
Ondrej Hryzak <ondrej@blindshell.com>
2024-08-15 16:32:58 +0200

    HW4 - start and stop led in while loop

--- lesson_4/homework.py
+++ lesson_4/homework.py
@@ -2,22 +2,39 @@ from neopixel import NeoPixel
 from microbit import pin0
 from microbit import sleep
 
-def zapni(poradi_led):
-    #TODO
+WHITE_COLOR = (255, 255, 255)
+OFF_COLOR = (0, 0, 0)
 
-def vypni(poradi_led):
-    #TODO
+led_map = {
+    "LED_FRONT_LEFT_IN": 0,
+    "LED_FRONT_LEFT_OUT": 1,
+    "LED_FRONT_RIGHT_OUT": 2,
+    "LED_FRONT_RIGHT_IN": 3,
+    "LED_REAR_RIGHT_IN": 4,
+    "LED_REAR_RIGHT_OUT": 5,
+    "LED_REAR_LEFT_OUT": 6,
+    "LED_REAR_LEFT_IN": 7
+}
 
-def nastav_barvu(poradi_led, barva):
-    #TODO
+np = NeoPixel(pin0, 8)
+led_index = led_map.get("LED_REAR_RIGHT_OUT")
 
-np = NeoPixel(pin0,8)
-#TODO: predelejte smycku tak, aby volala vyse definovane funkce
+def start_led(led_index):
+    set_led_color(led_index, WHITE_COLOR)
+
+def stop_led(led_index):
+    set_led_color(led_index, OFF_COLOR)
+
+def set_led_color(led_index, color):
+    np[led_index] = color
+    np.write()
+
 while True:
-    np[0] = (255,255,255) # nastavim prvni ledku, tzn [0] na bilou (RGB hodnoty)
-    np.write()
-    sleep(1000)
-    np[0] = (0,0,0) # nastavim prvni ledku, tzn [0] na cernou (RGB hodnoty)
-    np.write()
-    sleep(1000)
+    if led_index is not None:
+        start_led(led_index)
+        sleep(1000)
+        stop_led(led_index)
+        sleep(1000)
+    else:
+        print("Invalid LED name")
 
