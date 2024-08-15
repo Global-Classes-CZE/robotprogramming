0cc445f08fd424361f6520990ba8c91343c9d211
Jakub Šmíd <dzymator@gmail.com>
2024-08-15 12:28:04 +0200

    Jakub Šmíd DU4
    DU4

--- lesson_4/homework.py
+++ lesson_4/homework.py
@@ -2,22 +2,26 @@ from neopixel import NeoPixel
 from microbit import pin0
 from microbit import sleep
 
+barva = (255, 255, 255)
+
 def zapni(poradi_led):
-    #TODO
+    np[poradi_led] = (255, 255, 255)  # nastavim vybranou ledku na bilou (RGB hodnoty)
+    np.write()
 
 def vypni(poradi_led):
-    #TODO
+    np[poradi_led] = (0, 0, 0)  # nastavim vybranou ledku na cernou (RGB hodnoty)
+    np.write()
 
 def nastav_barvu(poradi_led, barva):
-    #TODO
+    np[poradi_led] = barva
+    np.write()
 
-np = NeoPixel(pin0,8)
-#TODO: predelejte smycku tak, aby volala vyse definovane funkce
+np = NeoPixel(pin0, 8)
+# TODO: predelejte smycku tak, aby volala vyse definovane funkce
+
 while True:
-    np[0] = (255,255,255) # nastavim prvni ledku, tzn [0] na bilou (RGB hodnoty)
-    np.write()
-    sleep(1000)
-    np[0] = (0,0,0) # nastavim prvni ledku, tzn [0] na cernou (RGB hodnoty)
-    np.write()
+    zapni(0)
     sleep(1000)
 
+    vypni(0)
+    sleep(1000)
