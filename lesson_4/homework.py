1caff0820e90352ac3c66d393f5f91e5f4533a54
JanChaloupek <jan.chaloupek@usmej.se>
2024-08-18 10:42:19 +0200

    Vylepseni kvuli praktickemu pouziti (odeslani LEDek az je to nutne)

--- lesson_4/homework.py
+++ lesson_4/homework.py
@@ -4,13 +4,14 @@ from microbit import sleep
 
 def zapni(poradi_led):
     nastav_barvu(poradi_led, (255, 255, 255))
+    np.write()
 
 def vypni(poradi_led):
     nastav_barvu(poradi_led, (0, 0, 0))
+    np.write()
 
 def nastav_barvu(poradi_led, barva):
     np[poradi_led] = barva
-    np.write()
 
 np = NeoPixel(pin0, 8)
 # TODO: predelejte smycku tak, aby volala vyse definovane funkce
