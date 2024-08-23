c7bda759ce48ae7c33e57dbc0de275349f59824d
Alexandr Ulybin <alexandr@ulybin.cz>
2024-08-23 07:57:39 +0200

    Oprava použití sleep

--- lesson_4/homework.py
+++ lesson_4/homework.py
@@ -5,12 +5,10 @@ from microbit import sleep
 def zapni(poradi_led):
     nastav_barvu(poradi_led, (255, 255, 255))
     np.write()
-    sleep(1000)
 
 def vypni(poradi_led):
     nastav_barvu(poradi_led, (0, 0, 0))
     np.write()
-    sleep(1000)
 
 def nastav_barvu(poradi_led, barva):
     np[poradi_led] = barva
@@ -19,4 +17,6 @@ np = NeoPixel(pin0, 8)
 
 while True:
     zapni(0)
+    sleep(1000)
     vypni(0)
+    sleep(1000)
