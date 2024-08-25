6a4f571e79d373603410a3a008cb52bbd93e85a9
Matus <matuskocik@gmail.com>
2024-08-25 14:52:14 +0200

    homework6(7) pokrocily

--- lesson_6/homework_pokrocily.py
+++ lesson_6/homework_pokrocily.py
@@ -7,17 +7,31 @@ from microbit import sleep
 # tzn dopredna_rychlost NENÍ v m/s, ale v nějakých nedefinovaných jednotkách…
 # tento problém odstraníme příští hodinu
 
+
 def jed(dopredna_rychlost, rotace):
     # TODO
-    d = 0  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
-    v_l = 0  # TODO vypocitejte dle prednasky 6
-    v_r = 0  # TODO vypocitejte dle prednasky 6
+    d = 0.15  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
+    v_l = dopredna_rychlost - (rotace * d / 2)  # TODO vypocitejte dle prednasky 6
+    v_r = dopredna_rychlost + (rotace * d / 2)  # TODO vypocitejte dle prednasky 6
 
     # !!!! POZOR !!!!
     # nez zadate v_l a v_r motorum, ujistete se,
     # ze hodnota je mezi 0-255
+    v_l = max(min(int(v_l), 255), 0)
+    v_r = max(min(int(v_r), 255), 0)
 
-     # TODO nastavte rychlost a smer motoru podle hodnot v_l a v_r
+    if v_l >= 0:
+        i2c.write(0x70, b"\x05" + bytes([v_l]))
+    else:
+        i2c.write(0x70, b"\x04" + bytes([abs(v_l)]))
+
+    if v_r >= 0:
+        i2c.write(0x70, b"\x03" + bytes([v_r]))
+    else:
+        i2c.write(0x70, b"\x02" + bytes([abs(v_r)]))
+
+    # TODO nastavte rychlost a smer motoru podle hodnot v_l a v_r
+
 
 if __name__ == "__main__":
 
@@ -26,3 +40,18 @@ if __name__ == "__main__":
     # pak rotoval okolo sve osy 1s s rychlosti "1350"  po dobu 1s
     # a pak zastavil a kod se vypnul
 
+    i2c.init(100000)
+    i2c.write(0x70, b"\x00\x01")
+    i2c.write(0x70, b"\xE8\xAA")
+
+    jed(135, 0)
+    sleep(1000)
+
+    jed(0, 0)
+    sleep(1000)
+
+    jed(0, 1350)
+    sleep(1000)
+
+    jed(0, 0)
+    sleep(1000)
