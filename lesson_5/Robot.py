5186a742b1bed13e3649ef309f476e6421571f3c
Ondrej Hryzak <ondrej@blindshell.com>
2024-08-25 23:27:47 +0200

    hw 5 - light and robot

--- lesson_5/Robot.py
+++ lesson_5/Robot.py
@@ -1,20 +1,67 @@
-import Svetlo
+from light import led_map, Light, OFF_COLOR, ORANGE_COLOR, WHITE_COLOR, RED_COLOR, BREAK_COLOR
+from neopixel import NeoPixel
+from microbit import pin0
+from microbit import sleep
 
 class Robot:
-    def __init__():
+    def __init__(self):
+        self.np = NeoPixel(pin0, 8)
+        self.blinker_left_front = Light(led_map["LED_FRONT_LEFT_OUT"], ORANGE_COLOR, self.np)
+        self.blinker_right_front = Light(led_map["LED_FRONT_RIGHT_OUT"], ORANGE_COLOR, self.np)
+        self.blinker_left_rear = Light(led_map["LED_REAR_LEFT_OUT"], ORANGE_COLOR, self.np)
+        self.blinker_right_rear = Light(led_map["LED_REAR_RIGHT_OUT"], ORANGE_COLOR, self.np)
 
-    def indikuj(smer_zataceni):
-        #pro ted je smer_zataceni string "doprava" nebo "doleva"
-        #TODO - blikejte s vnejsimi ledkami oranzovou barvou, zvolte periodu blikani
+        self.led_rear_left_in = Light(led_map["LED_REAR_LEFT_IN"], OFF_COLOR, self.np)
+        self.led_rear_right_in = Light(led_map["LED_REAR_RIGHT_IN"], OFF_COLOR, self.np)
 
-    def zapni_svetla():
-        #TODO zapnete predni svetla (vnitrni LED) a zadni svetla - zda doporucuji barvu (60,0,0)
-
-    def brzdi():
-        #TODO zadni svetla by mela svitit barvou (255,0,0)
-
-    def vypni_svetla():
-        #TODO
+        self.led_front_left = Light(led_map["LED_FRONT_LEFT_IN"], WHITE_COLOR, self.np)
+        self.led_front_right = Light(led_map["LED_FRONT_RIGHT_IN"], WHITE_COLOR, self.np)
 
 
+    def indikuj(self, direction):
+        if(direction=="right"):
+            self.blinker_right_front.start_led()
+            self.blinker_right_rear.start_led()
+            sleep(1000)
+            self.blinker_right_front.stop_led()
+            self.blinker_right_rear.stop_led()
+            sleep(1000)
+        elif(direction=="left"):
+            self.blinker_left_front.start_led()
+            self.blinker_left_rear.start_led()
+            sleep(1000)
+            self.blinker_left_front.stop_led()
+            self.blinker_left_rear.stop_led()
+            sleep(1000)
+        else:
+            self.blinker_left_front.stop_led()
+            self.blinker_left_rear.stop_led()
+            
+
+    def turn_on_lights(self):
+        self.led_front_left.start_led()
+        self.led_front_right.start_led()
+        self.led_rear_left_in.set_led_color(RED_COLOR)
+        self.led_rear_right_in.set_led_color(RED_COLOR)
+        self.led_rear_right_in.start_led()
+        self.led_rear_left_in.start_led()
+
+    def breaking(self):
+        left_color = self.led_rear_left_in.color
+        right_color = self.led_rear_right_in.color
+        self.led_rear_left_in.set_led_color(BREAK_COLOR)
+        self.led_rear_right_in.set_led_color(BREAK_COLOR)
+        self.led_rear_left_in.start_led()
+        self.led_rear_right_in.start_led()
+        sleep(1000)
+        self.led_rear_left_in.set_led_color(left_color)
+        self.led_rear_right_in.set_led_color(right_color)
+        self.led_rear_left_in.start_led()
+        self.led_rear_right_in.start_led()
+
+    def turn_off_lights(self):
+        self.led_front_left.stop_led()
+        self.led_front_right.stop_led()
+        self.led_rear_left_in.stop_led()
+        self.led_rear_right_in.stop_led()
 
---
+++ lesson_5/light.py
@@ -0,0 +1,34 @@
+WHITE_COLOR = (255, 255, 255)
+ORANGE_COLOR = (251, 163, 26)
+BREAK_COLOR = (255, 0, 0)
+RED_COLOR = (60, 0, 0)
+OFF_COLOR = (0, 0, 0)
+
+led_map = {
+    "LED_FRONT_LEFT_IN": 0,
+    "LED_FRONT_LEFT_OUT": 1,
+    "LED_FRONT_RIGHT_OUT": 2,
+    "LED_FRONT_RIGHT_IN": 3,
+    "LED_REAR_RIGHT_IN": 4,
+    "LED_REAR_RIGHT_OUT": 5,
+    "LED_REAR_LEFT_OUT": 6,
+    "LED_REAR_LEFT_IN": 7
+}
+
+class Light:
+    def __init__(self, led_index, color, np):
+        self.led_index = led_index
+        self.color = color
+        self.np = np
+    
+    def start_led(self):
+        self.np[self.led_index] = self.color
+        self.np.show()
+
+    def stop_led(self):
+        self.np[self.led_index] = OFF_COLOR
+        self.np.show()
+
+    def set_led_color(self, color):
+        self.color = color
+        
