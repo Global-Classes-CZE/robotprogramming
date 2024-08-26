dfd84389d6a7690210f4e99a667ae133ae32c539
aihatn <chrastinakamil@gmail.com>
2024-08-26 21:21:48 +0200

    Kamil Chrastina DU7 - Pokročilý pt2
    DU7 pokročilý part 2

--- lesson_6/homework_pokrocily_part2.py
+++ lesson_6/homework_pokrocily_part2.py
@@ -4,6 +4,8 @@ from microbit import pin15
 from microbit import button_a
 
 tiky = 0  # globalni promena
+pe_predchozi_stav = 0
+le_predchozi_stav = 0
 
 def enkoder_signal(jmeno_enkoderu):
     if jmeno_enkoderu == "pravy_enkoder":
@@ -16,6 +18,23 @@ def enkoder_signal(jmeno_enkoderu):
 
 def pocet_tiku(jmeno_enkoderu):
     # TODO volejte funkce enkoder_signal a pocitejte nove tiky
+    global tiky
+    global pe_predchozi_stav
+    global le_predchozi_stav
+
+    if jmeno_enkoderu == "pravy_enkoder":
+        pe_aktualni_stav = enkoder_signal("pravy_enkoder")
+
+        if pe_aktualni_stav != pe_predchozi_stav:
+            tiky += 1
+            pe_predchozi_stav = pe_aktualni_stav
+
+    if jmeno_enkoderu == "levy_enkoder":
+        le_aktualni_stav = enkoder_signal("levy_enkoder")
+
+        if le_aktualni_stav != le_predchozi_stav:
+            tiky += 1
+            le_predchozi_stav = le_aktualni_stav
 
     return tiky
 
@@ -23,11 +42,7 @@ if __name__ == "__main__":
 
    # misto vypisu vidi/nevidi vypisujte tiky
     while not button_a.was_pressed():
-        data_enkoderu = enkoder_signal("levy_enkoder")
-        if data_enkoderu == 1:
-            print("levy enkoder vidi")
-        elif data_enkoderu == 0:
-            print("levy enkoder nevidi")
-        else:
-            print("jsem tululum a upsala jsem se nekde v nazvu enkoderu :)")
-        sleep(100)
+        pocet_tiku("pravy_enkoder")
+        pocet_tiku("levy_enkoder")
+        print(int(tiky))
+        sleep(10)
