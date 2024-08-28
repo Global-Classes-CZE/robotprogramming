2f8974192d97f30820710502458949962451971c
Alexandr Ulybin <alexandr@ulybin.cz>
2024-08-28 09:12:49 +0200

    Update homework_pokrocily_part2.py

--- lesson_6/homework_pokrocily_part2.py
+++ lesson_6/homework_pokrocily_part2.py
@@ -4,6 +4,8 @@ from microbit import pin15
 from microbit import button_a
 
 tiky = 0  # globalni promena
+data = 0
+
 
 def enkoder_signal(jmeno_enkoderu):
     if jmeno_enkoderu == "pravy_enkoder":
@@ -14,20 +16,22 @@ def enkoder_signal(jmeno_enkoderu):
         print("Zadali jste nepodporovane jmeno")
         return -1
 
+
 def pocet_tiku(jmeno_enkoderu):
     # TODO volejte funkce enkoder_signal a pocitejte nove tiky
-
+    global tiky
+    global data
+    data_enkoderu = enkoder_signal(jmeno_enkoderu)
+    if data != data_enkoderu:
+        tiky += 1
+        data = data_enkoderu
     return tiky
+
 
 if __name__ == "__main__":
 
-   # misto vypisu vidi/nevidi vypisujte tiky
+    # misto vypisu vidi/nevidi vypisujte tiky
     while not button_a.was_pressed():
-        data_enkoderu = enkoder_signal("levy_enkoder")
-        if data_enkoderu == 1:
-            print("levy enkoder vidi")
-        elif data_enkoderu == 0:
-            print("levy enkoder nevidi")
-        else:
-            print("jsem tululum a upsala jsem se nekde v nazvu enkoderu :)")
+        pocet_tiku("levy_enkoder")
+        print(tiky)
         sleep(100)
