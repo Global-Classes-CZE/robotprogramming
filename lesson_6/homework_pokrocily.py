64413ee6b759a14d0e937672cdbcb1fa20a80e6d
Niker313 <kucik.honza@gmail.com>
2024-08-28 20:28:12 +0200

    Update homework_pokrocily.py

--- lesson_6/homework_pokrocily.py
+++ lesson_6/homework_pokrocily.py
@@ -9,15 +9,35 @@ from microbit import sleep
 
 def jed(dopredna_rychlost, rotace):
     # TODO
-    d = 0  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
-    v_l = 0  # TODO vypocitejte dle prednasky 6
-    v_r = 0  # TODO vypocitejte dle prednasky 6
+    d = 0.075  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
+    v_l = dopredna_rychlost - d*rotace
+    v_r = dopredna_rychlost + d*rotace
+    #print(v_l, v_r)
 
     # !!!! POZOR !!!!
     # nez zadate v_l a v_r motorum, ujistete se,
     # ze hodnota je mezi 0-255
 
      # TODO nastavte rychlost a smer motoru podle hodnot v_l a v_r
+    if 0 <= v_l <= 255:
+        i2c.write(0x70, b"\x05" + bytes([int(v_l)]))
+    elif -255 <= v_l < 0:
+        v_l = v_l*-1
+        i2c.write(0x70, b"\x04" + bytes([int(v_l)]))
+    if 0 <= v_r <= 255:
+        i2c.write(0x70, b"\x03" + bytes([int(v_r)]))
+    elif -255 <= v_r < 0:
+        v_r = v_r*-1
+        i2c.write(0x70, b"\x02" + bytes([int(v_r)]))
+    else:
+        raise ValueError("PWM mimo rozsah - v_l=",v_l,", v_r=",v_r)
+
+# musel jsem udelat tuto funkci pro zastaveni, protoze na konci programu mi jed(0,0) nevypinalo leve kolo
+def zastav():
+    i2c.write(0x70, b"\x05\x00")
+    i2c.write(0x70, b"\x04\x00")
+    i2c.write(0x70, b"\x03\x00")
+    i2c.write(0x70, b"\x02\x00")
 
 if __name__ == "__main__":
 
@@ -25,4 +45,20 @@ if __name__ == "__main__":
     # pak zastavil na 1s
     # pak rotoval okolo sve osy 1s s rychlosti "1350"  po dobu 1s
     # a pak zastavil a kod se vypnul
+    i2c.init(freq=100000)
+    i2c.write(0x70, b"\x00\x01")
+    i2c.write(0x70, b"\xE8\xAA")
+    sleep(100)
+    i2c.write(0x70, b"\x05\x00")
+    i2c.write(0x70, b"\x04\x00")
+    i2c.write(0x70, b"\x03\x00")
+    i2c.write(0x70, b"\x02\x00")
+
+    jed(135, 0)
+    sleep(1000)
+    zastav()
+    sleep(1000)
+    jed(0, 1350)
+    sleep(1000)
+    zastav()
 
