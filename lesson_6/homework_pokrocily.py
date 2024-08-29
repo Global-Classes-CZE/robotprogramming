929c873d27d0932263088ae494f7d505efa20415
Jakub Šmíd <dzymator@gmail.com>
2024-08-29 20:53:10 +0200

    Jakub Smid DU6 pokrocily 1

--- lesson_6/homework_pokrocily.py
+++ lesson_6/homework_pokrocily.py
@@ -1,3 +1,4 @@
+# Write your code here :-)
 from microbit import i2c
 from microbit import sleep
 
@@ -8,16 +9,46 @@ from microbit import sleep
 # tento problém odstraníme příští hodinu
 
 def jed(dopredna_rychlost, rotace):
-    # TODO
-    d = 0  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
-    v_l = 0  # TODO vypocitejte dle prednasky 6
-    v_r = 0  # TODO vypocitejte dle prednasky 6
-
+                                                # TODO
+    d = 0.075                                   # tuto hodnotu si musite zmerit na robotovi, zadejte to v m - 7,5 cm = 0,075 m
+    v_l = abs(int(dopredna_rychlost-d*rotace))  # TODO vypocitejte dle prednasky 6
+    v_r = abs(int(dopredna_rychlost+d*rotace))  # TODO vypocitejte dle prednasky 6
+    
     # !!!! POZOR !!!!
     # nez zadate v_l a v_r motorum, ujistete se,
     # ze hodnota je mezi 0-255
 
+    if (0 <= abs(v_l) <= 255) and (0 <= abs(v_r) <= 255):
+        if dopredna_rychlost > 0:
+            i2c.write(0x70, b"\x03" + bytes([v_r]))
+            i2c.write(0x70, b"\x05" + bytes([v_l]))
+        elif dopredna_rychlost < 0:
+            i2c.write(0x70, b"\x02" + bytes([v_r]))
+            i2c.write(0x70, b"\x04" + bytes([v_l]))
+        elif dopredna_rychlost == 0 and rotace >= 0:
+            i2c.write(0x70, b"\x03" + bytes([rotace]))
+            i2c.write(0x70, b"\x04" + bytes([rotace]))
+        elif dopredna_rychlost == 0 and rotace < 0:
+            i2c.write(0x70, b"\x02" + bytes([abs(rotace)]))
+            i2c.write(0x70, b"\x05" + bytes([abs(rotace)]))
+            
+    else:
+        print("hodnoty mimo rozsah")
+        
+    print()
+
+    # 02 - pravý vzad
+    # 03 - pravý vpřed
+    # 04 - levý vzad
+    # 05 - levý vpřed
+
      # TODO nastavte rychlost a smer motoru podle hodnot v_l a v_r
+
+def zastav():
+    i2c.write(0x70, b"\x02" + bytes([0]))
+    i2c.write(0x70, b"\x03" + bytes([0]))
+    i2c.write(0x70, b"\x04" + bytes([0]))
+    i2c.write(0x70, b"\x05" + bytes([0]))
 
 if __name__ == "__main__":
 
@@ -25,4 +56,25 @@ if __name__ == "__main__":
     # pak zastavil na 1s
     # pak rotoval okolo sve osy 1s s rychlosti "1350"  po dobu 1s
     # a pak zastavil a kod se vypnul
+
+    #zapni komunikaci
+    i2c.init(freq=100000)
+
+    # probuď motory
+    i2c.write(0x70, b"\x00\x01")
+    i2c.write(0x70, b"\xE8\xAA")
+    
+    
+    #jedeme
+
+    jed(135, 0)    
+    sleep(1000)
+    
+    zastav()
+    sleep(1000)
+
+    jed(0,135)
+    sleep(1000)
+
+    zastav()
 
