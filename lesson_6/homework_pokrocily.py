9275bb5efdeabd529a9ad108f699334ee1a446c8
Martin Dostál <martin.dostal@redbit.cz>
2024-08-29 18:24:50 +0200

    DU7 part 1

--- lesson_6/homework_pokrocily.py
+++ lesson_6/homework_pokrocily.py
@@ -1,28 +1,48 @@
 from microbit import i2c
 from microbit import sleep
+from microbit import pin19
+from microbit import pin20
 
-# dopredna_rychlost v je float
-# rotace je uhlova rychlost omega a take float
-# Poznámka: samotná velikost čísel ještě nebude dávat smysl,
-# tzn dopredna_rychlost NENÍ v m/s, ale v nějakých nedefinovaných jednotkách…
-# tento problém odstraníme příští hodinu
+i2c.init(freq=400000, sda=pin20, scl=pin19)
+i2c.write(0x70, b'\x00\x01')
+i2c.write(0x70, b'\xE8\xAA')
 
 def jed(dopredna_rychlost, rotace):
-    # TODO
-    d = 0  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
-    v_l = 0  # TODO vypocitejte dle prednasky 6
-    v_r = 0  # TODO vypocitejte dle prednasky 6
+    d = 0.074  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
+    v_l = dopredna_rychlost - d * rotace   # TODO vypocitejte dle prednasky 6
+    v_r = dopredna_rychlost + d * rotace  # TODO vypocitejte dle prednasky 6
 
-    # !!!! POZOR !!!!
-    # nez zadate v_l a v_r motorum, ujistete se,
-    # ze hodnota je mezi 0-255
+    zastav()
 
-     # TODO nastavte rychlost a smer motoru podle hodnot v_l a v_r
+    if v_l < -255 or v_l > 255:
+        return -1
+
+    if v_r < -255 or v_r > 255:
+        return -1
+
+    if(v_l <= 0):
+        i2c.write(0x70, b'\x02' + bytes([abs(int(v_l))]))
+    else:
+        i2c.write(0x70, b'\x03' + bytes([abs(int(v_l))]))
+
+    if(v_r <= 0):
+        i2c.write(0x70, b'\x04' + bytes([abs(int(v_r))]))
+    else:
+        i2c.write(0x70, b'\x05' + bytes([abs(int(v_r))]))
+
+def zastav():
+    i2c.write(0x70, b'\x02' + bytes([0]))
+    i2c.write(0x70, b'\x03' + bytes([0]))
+    i2c.write(0x70, b'\x04' + bytes([0]))
+    i2c.write(0x70, b'\x05' + bytes([0]))
 
 if __name__ == "__main__":
-
-    # TODO napiste kod, aby robot jel rychlosti "135" 1s vpred
-    # pak zastavil na 1s
-    # pak rotoval okolo sve osy 1s s rychlosti "1350"  po dobu 1s
-    # a pak zastavil a kod se vypnul
+    zastav()
+    jed(135, 0)
+    sleep(1000)
+    zastav()
+    sleep(1000)
+    jed(0, 1350)
+    sleep(1000)
+    zastav()
 
