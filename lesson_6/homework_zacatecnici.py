c2b4b744c81d45cd158772a24d8235babb125a19
Martin Slavkovsky <xslavkov@gmail.com>
2024-08-30 22:00:58 +0200

    zacatecnici

--- lesson_6/homework_zacatecnici.py
+++ lesson_6/homework_zacatecnici.py
@@ -6,11 +6,24 @@ from microbit import sleep
 # Rychlost je celociselne cislo od 0-255
 # Napoveda: budete potrebovat podminky if/else v kodu
 def jed(motor, smer, rychlost):
-    # TODO
+    rychlost = min(rychlost, 255)
+    rychlost = max(rychlost, 0)
+    if motor == "pravy":
+        if smer == "dopredu":
+            i2c.write(0x70, b"\x03" + bytes([rychlost]))
+        else:
+            i2c.write(0x70, b"\x02" + bytes([rychlost]))
+    else:
+        if smer == "dopredu":
+            i2c.write(0x70, b"\x05" + bytes([rychlost]))
+        else:
+            i2c.write(0x70, b"\x04" + bytes([rychlost]))
+
 
 # Motor je typu string a muze mit hodnoty “pravy” a “levy”
 def zastav(motor):
-    # TODO
+    jed(motor, 'dopredu', 0)
+    jed(motor, 'dozadu', 0)
 
 if __name__ == "__main__":
     i2c.init(freq=100000)
@@ -20,15 +33,19 @@ if __name__ == "__main__":
     i2c.write(0x70, b"\xE8\xAA")
 
     # TODO predelelejte tento kod, aby robot jel 1s vpred
+    jed("pravy", "dopredu", 128)
+    jed("levy", "dopredu", 128)
+    sleep(1000)
+    
     # pak zastavil na 1s
+    zastav("pravy")
+    zastav("levy")
+    sleep(1000)
+    
     # pak se rozjel vzad po dobu 1s
+    jed("pravy", "dozadu", 128)
+    jed("levy", "dozadu", 128)
+    sleep(1000)
     # a pak zastavil a kod se vypnul
-    # 0x02, 0x03
-    # 0-255
-    sleep(100)
-    i2c.write(0x70, b"\x05" + bytes([135]))
-    sleep(2000)
-    i2c.write(0x70, b"\x02" + bytes([0]))
-    i2c.write(0x70, b"\x03" + bytes([0]))
-    i2c.write(0x70, b"\x04" + bytes([0]))
-    i2c.write(0x70, b"\x05" + bytes([0]))
+    zastav("pravy")
+    zastav("levy")
