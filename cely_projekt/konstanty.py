0ca7e75c300a8e28940eb35f7380cb66b6fd76f0
Lenka Mudrich <lenka.mudrich@gmail.com>
2024-09-04 10:29:47 +0100

    Pridany dalsi definice

--- cely_projekt/konstanty.py
+++ cely_projekt/konstanty.py
@@ -6,3 +6,18 @@ class Konstanty:
 
     DOPREDU = "dopredu"
     DOZADU = "dozadu"
+
+    ENKODER = "enkoder"
+    PR_ENKODER = PRAVY + "_" + ENKODER
+    LV_ENKODER = LEVY + "_" + ENKODER
+
+    IR = "IR"
+    PR_IR = PRAVY + "_" + IR
+    LV_IR = LEVY + "_" + IR
+
+    SENZOR_CARY = "senzor_cary"
+    PR_S_CARY = PRAVY + "_" + SENZOR_CARY
+    LV_S_CARY = LEVY + "_" + SENZOR_CARY
+    PROS_S_CARY = "prostredni_" + SENZOR_CARY
+
+    PI = 3.14159265359
