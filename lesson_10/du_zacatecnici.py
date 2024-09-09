6935b912ef327003ab94c8f5ffbdfbc7dd54933b
pavolbutora <pavolbutora@gmail.com>
2024-09-09 23:19:23 +0200

    Pavol Butora DU10

--- lesson_10/du_zacatecnici.py
+++ lesson_10/du_zacatecnici.py
@@ -1,12 +1,28 @@
 from senzory import Senzory
+from microbit import button_a
+from konstanty import Konstanty
 
 class Naraznik:
 
     # jmeno muze byt jen levy_IR a nebo pravy_IR
     def __init__(self, jmeno):
-    # TODO
+        if jmeno != Konstanty.LV_IR and jmeno != Konstanty.PR_IR:
+            return -1
+
+        self.jmeno = jmeno
+        self.senzory = Senzory(True)
 
     def narazil_jsem(self):
-    # TODO
-    # navratova hodnota True - narazil, False - nenarazil
+        data_Senzorov = self.senzory.precti_senzory()
+
+        if int(data_Senzorov[Konstanty.LV_IR]) == 1:
+            return False
+        else:
+            return True
+
+if __name__ == "__main__":
+    levy_naraznik = Naraznik(Konstanty.LV_IR)
+
+    while not button_a.was_pressed():
+        print(levy_naraznik.narazil_jsem())
 
