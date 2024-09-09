ba45bc5bbf65f164d2b9d2b6e247b446d1236ba3
Martin Slavkovsky <xslavkov@gmail.com>
2024-09-09 23:15:37 +0200

    joi light example refactored

--- lesson_9/joi-light-example-refactored.py
+++ lesson_9/joi-light-example-refactored.py
@@ -1,70 +1,30 @@
-from microbit import *
-import neopixel
-from utime import ticks_ms, ticks_diff
+from microbit import sleep
+from Light import Light, Color
 
-
-np = neopixel.NeoPixel(pin0, 8)
-headlights = (0, 3)
-backlights = (5, 6)
-led_white = (60, 60, 60)
-led_red = (60, 0, 0)
-led_off = (0, 0, 0)
-led_red_br = (255, 0, 0)
-led_orange = (100, 35, 0)
-indicator_left = (1, 4)
-indicator_right = (2, 7)
-indicator_warning = (1, 2, 4, 7)
-
+light = Light()
 
 def lightsON():
-    for x in headlights:
-        np[x] = led_white
-    for x in backlights:
-        np[x] = led_red
-    np.show()
+    light.on(Light.FRONT.BOTH, Color.WHITE_50)
+    light.on(Light.BACK.BOTH, Color.RED_50)
 
 def lightsOFF():
-    for x in headlights:
-        np[x] = led_off
-    for x in backlights:
-        np[x] = led_off
-    np.show()
+    light.off(Light.FRONT.BOTH)
+    light.off(Light.BACK.BOTH)
 
 def lightsBreakON():
-    temp = np[backlights[1]]
-    for x in backlights:
-        np[x] = led_red_br
-    np.show()
-    return (temp)
+    light.on(Light.BACK.BOTH, Color.RED_100)
 
 def lightsBreakOFF():
-    for x in backlights:
-        np[x] = led_off
-    np.show()
+    light.off(Light.BACK.BOTH)
 
 def lightsBackON():
-    temp = np[backlights[0]]
-    np[backlights[0]] = led_white
-    np.show()
-    return (temp)
+    light.on(Light.BACK.BOTH, Color.WHITE_50)
 
 def lightsBackOFF():
-    np[backlights[0]] = led_off
-    np.show()
+    light.off(Light.BACK.BOTH)
 
-def lightsIndicator(direction, last_ind_act):
-    if ticks_diff(ticks_ms(), last_ind_act) >= 400 and np[direction[0]] == led_off:
-        for x in direction:
-            np[x] = led_orange
-        np.show()
-        return ticks_ms()
-    elif ticks_diff(ticks_ms(), last_ind_act) >= 400 and np[direction[0]] == led_orange:
-        for x in direction:
-            np[x] = led_off
-        np.show()
-        return ticks_ms()
-    else:
-        return last_ind_act
+def lightsIndicator(direction):
+    light.blink(direction, Color.YELLOW, 2500)
 
 while True:
     # Application example
@@ -79,4 +39,8 @@ while True:
     lightsBackOFF()  #Reversing lights off
     sleep(1000)
     lightsOFF()  #Light off
+    sleep(1000)
+    lightsIndicator(Light.FRONT.LEFT)
+    sleep(1000)
+    lightsIndicator(Light.BACK.LEFT)
     sleep(1000)
