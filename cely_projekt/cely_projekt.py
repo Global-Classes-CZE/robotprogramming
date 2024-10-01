1b3451ae00d251b87135f7a52bf807fdd0302b18
Lenka Mudrich <lenka.mudrich@gmail.com>
2024-10-01 16:35:46 +0100

    Pridany tridy svetel a jizda po care

--- cely_projekt/cely_projekt.py
+++ cely_projekt/cely_projekt.py
@@ -1,5 +1,8 @@
 from microbit import i2c, sleep
-from microbit import pin14, pin15
+from microbit import pin14, pin15, pin0
+from microbit import display
+
+from neopixel import NeoPixel
 
 from utime import ticks_us, ticks_diff
 
@@ -26,6 +29,19 @@ class Konstanty:
     PROS_S_CARY = "prostredni_" + SENZOR_CARY
 
     PI = 3.14159265359
+
+    CARA = "cara"
+    KRIZOVATKA = "krizovatka"
+    ZTRACEN = "ztracen"
+    ZATOC = "zatoc"
+
+    ROVNE = "rovne"
+    VPRAVO = "vpravo"
+    VLEVO = "vlevo"
+    VZAD = "vzad"
+
+    VSE = "vse"
+
 
 class Senzory:
 
@@ -103,7 +119,6 @@ class Enkoder:
             else:
                 return -2
 
-    # drive se tato metoda jmenovala pocet_tiku
     def aktualizuj_se(self):
         if self.__DEBUG:
             print("v aktualizuj", self.__tiky)
@@ -165,8 +180,6 @@ class Motor:
         self.aktualni_rychlost = 0
 
     def inicializuj(self):
-        # self.enkoder.inicializuj()
-        # probud cip motoru
         i2c.write(0x70, b"\x00\x01")
         i2c.write(0x70, b"\xE8\xAA")
 
@@ -186,9 +199,6 @@ class Motor:
         self.__cas_posledni_regulace = ticks_us()
 
     def jed_doprednou_rychlosti(self, v: float):
-        """
-        Rozjede motor pozadovanou doprednou rychlosti
-        """
         if not self.__inicializovano:
             return -1
 
@@ -198,9 +208,6 @@ class Motor:
 
         self.__rychlost_byla_zadana = True
 
-        # pouziji funkci abs pro vypocteni absolutni hodnoty
-        # PWM je vzdy pozitivni
-        # znamenko uhlove rychlosti ovlivni smer
         prvni_PWM = self.__uhlova_na_PWM(abs(self.__pozadovana_uhlova_r_kola))
         if self.__DEBUG:
             print("prvni_PWM", prvni_PWM)
@@ -215,27 +222,21 @@ class Motor:
         return self.__jed_PWM(prvni_PWM)
 
 
-    #lekce 8, slidy 8 - 11
     def __dopredna_na_uhlovou(self, v: float):
-        """
-        Prepocita doprednou rychlost kola na uhlovou
-        """
         return v/(self.__prumer_kola/2)
 
     def __uhlova_na_PWM(self, uhlova):
-        """
-        Prepocte uhlovou rychlost na PWM s vyuzitim dat z kalibrace
-        """
 
         a, b = self.__najdi_spravne_parametery(uhlova)
-        return int(a*uhlova + b)
+        if uhlova == 0: #TODO uvazuj, zda tohle by nemelo byt pod min rozjezd rychlost
+            return 0
+        else:
+            return int(a*uhlova + b)
 
     def __najdi_spravne_parametery(self, uhlova):
         # TODO
         return self.__primky_par_a[0], self.__primky_par_b[0]
 
-    # DU 5 pokrocily/DU 7 zacatecnici
-    # jed(motor, smer, rychlost)
     def __jed_PWM(self, PWM):
         je_vse_ok = -2
         if self.__smer == Konstanty.DOPREDU:
@@ -270,7 +271,6 @@ class Motor:
 
         return navratova_hodnota
 
-    # ukazka v pridanem materialu k hodine 8
     def __reguluj_otacky(self):
 
         if not self.__inicializovano:
@@ -282,8 +282,7 @@ class Motor:
         P = 6
 
         self.aktualni_rychlost = self.__enkoder.vypocti_rychlost()
-        # aktualni_rychlost bude vzdy pozitivni
-        # musim tedy kombinovat se smerem, kterym se pohybuji
+
         if self.__pozadovana_uhlova_r_kola < 0:
             self.aktualni_rychlost *= -1
 
@@ -292,33 +291,16 @@ class Motor:
         return self.__zmen_PWM_o(akcni_zasah)
 
     def __zmen_PWM_o(self, akcni_zasah):
-        # error a tim padem i akcni_zasah muze byt jak pozitivni tak negativni, nezavisle na smeru
-        # priklad, pozadovana = 5, aktualni = 2
-        # smer = dopredu a error > 0 => musim zrychlit
-        # priklad, pozadovana 5, aktualni 10
-        # smer = dopredu a error < 0 => musim zpomalit
-        # priklad, pozadovana -5, aktualni -2
-        # smer = dozadu a error < 0 => musim zrychlist
-        # priklad, pozadovana -5, aktualni -7
-        # smer= dozadu a error>0 => musim zpomalit
 
-        akcni_zasah = int(akcni_zasah) #PWM je v celych cislech
+        akcni_zasah = int(akcni_zasah)
 
         if self.__smer == Konstanty.DOZADU:
-            # prohod logiku aby pozitivni akcni_zasah u jizdy dozadu
-            # take znamenal zrychli a ne zpomal, viz priklad nahore
             akcni_zasah *= -1
 
         nove_PWM = self.__PWM + akcni_zasah
 
         if nove_PWM > 255:
             nove_PWM = 255
-
-        # toto nastane ve chvili, kdy by akcni_zasah zpomaleni byl
-        # vetsi nez aktualni rychlost
-        # myslenka je, ze chceme zpomalit co nejvice muzeme, tedy v nasem pripade zastavit
-        # rozjet motor na opacnou stranu za me z pohledu rizeni rychlosti otaceni nedava smysl
-        # ale muzeme se o tom pobavit :)
 
         if nove_PWM < 0:
             nove_PWM  = 0
@@ -327,7 +309,7 @@ class Motor:
 
 class Robot:
 
-    def __init__(self, rozchod_kol: float, prumer_kola: float, nova_verze=True):
+    def __init__(self, rozchod_kol: float, prumer_kola: float, prikazy, nova_verze=True):
         """
         Konstruktor tridy
         """
@@ -339,12 +321,28 @@ class Robot:
         self.__inicializovano = False
         self.__cas_minule_reg = ticks_us()
         self.__perioda_regulace = 1000000
+        self.__senzory = Senzory(nova_verze)
+
+        self.__perioda_cary_us = 75000
+
+        self.__prikazy = prikazy
+        self.__index_prikazu = 0
+
+        self.__posledni_cas_popojeti = 0
+
+        self.svetla = SvetelnyModul()
 
     def inicializuj(self):
         i2c.init(400000)
         self.__levy_motor.inicializuj()
         self.__pravy_motor.inicializuj()
         self.__inicializovano = True
+
+        self.__posledni_cas_reg_cary_us = ticks_us()
+
+        self.jed(0,0)
+        self.svetla.zapni_obrysova()
+        return True
 
     # pokrocily ukol 7
     def jed(self, dopredna_rychlost: float, uhlova_rychlost: float):
@@ -382,17 +380,176 @@ class Robot:
 
         if ticks_diff(ticks_us(), self.__cas_minule_reg) > self.__perioda_regulace:
             self.__cas_minule_reg = ticks_us()
-            #self.__reguluj()
 
-    def __reguluj(self):
+    def vycti_senzory_cary(self):
 
-        v, omega = self.__aktualni_rychlost()
-        error_omega = self.__uhlova_rychlost - omega
-        error_v = self.__dopredna_rychlost - v
+        senzoricka_data = self.__senzory.precti_senzory()
 
-        P_om = 1
-        P_v = 1
-        u_om = P_om * error_omega
-        u_v = P_v * error_v
-        print("reguluju", u_v, u_om)
-        self.jed(u_v, u_om)
+        if senzoricka_data[Konstanty.LV_S_CARY] and senzoricka_data[Konstanty.PR_S_CARY]:
+            return Konstanty.KRIZOVATKA
+        if senzoricka_data[Konstanty.LV_S_CARY] and senzoricka_data[Konstanty.PROS_S_CARY]:
+            return Konstanty.KRIZOVATKA
+        if senzoricka_data[Konstanty.PROS_S_CARY] and senzoricka_data[Konstanty.PR_S_CARY]:
+            return Konstanty.KRIZOVATKA
+
+        elif not senzoricka_data[Konstanty.LV_S_CARY] and not senzoricka_data[Konstanty.PR_S_CARY] and not senzoricka_data[Konstanty.PROS_S_CARY]:
+            return Konstanty.ZTRACEN
+        else:
+            return Konstanty.CARA
+
+    def jed_po_care(self, dopredna, uhlova):
+        cas_ted = ticks_us()
+
+        if ticks_diff(cas_ted, self.__posledni_cas_reg_cary_us) > self.__perioda_cary_us:
+            self.__posledni_cas_reg_cary_us = cas_ted
+            data = self.__senzory.precti_senzory()
+
+            if data[Konstanty.LV_S_CARY]:
+                self.jed(dopredna, uhlova)
+
+            if data[Konstanty.PR_S_CARY]:
+                self.jed(dopredna, -uhlova)
+
+            if not data[Konstanty.LV_S_CARY] and not data[Konstanty.PR_S_CARY]:
+                self.jed(dopredna, 0)
+
+class Obrazovka:
+    def pis(text, displej=True):
+        if displej:
+            display.show(text[0])
+        else:
+            print(text)
+
+class Svetlo:
+    def __init__(self, poradi_led, neopixel_pole, barva):
+        self.poradi = poradi_led
+        self.barva = barva
+        self.np = neopixel_pole
+
+    def zapni(self):
+        self.nastav_barvu(self.barva)
+        self.np.write()
+
+    def vypni(self):
+        self.nastav_barvu((0, 0, 0))  #nastavim ledku na dane pozici na cernou (RGB hodnoty)
+        self.np.write()
+
+    def nastav_barvu(self, barva):
+        self.np[self.poradi] = barva  #nastavim ledku na dane pozici na zadanou barvu (RGB hodnoty)
+
+class PredniSvetlo(Svetlo):
+    def __init__(self, poradi_led, neopixel):
+        super().__init__(poradi_led, neopixel, (60, 60, 60))
+        self.vypni()
+
+    def zapni_dalkove(self):
+        self.nastav_barvu((255, 255, 255))
+        self.np.write()
+
+class Blinkr(Svetlo):
+    def __init__(self, poradi_led, neopixel):
+        super().__init__(poradi_led, neopixel, (100, 35, 0))
+        self.cas_minule = ticks_us()
+        self.perioda_blikani = 500000  # cas v us
+        self.vypni()
+        self.zapnuto = False
+
+    def blikej(self):
+        cas_ted = ticks_us()
+        kolik_ubehlo_od_minula = ticks_diff(cas_ted, self.cas_minule)
+
+        if kolik_ubehlo_od_minula > self.perioda_blikani:
+            # bliknuti znamena zmenit stav - kdyz je ledka zapla, tak ji chceme vypnout
+            if self.zapnuto:
+                self.vypni()
+                self.zapnuto = False
+            else:
+                self.zapni()
+                self.zapnuto = True
+
+            self.cas_minule = cas_ted
+
+class ZadniSvetlo(Svetlo):
+    def __init__(self, poradi_led, neopixel):
+        super().__init__(poradi_led, neopixel, (60, 0, 0))
+        self.vypni()
+
+    def zapni_brzdove():
+        self.nastav_barvu(poradi_led, (255, 0, 0))
+        self.np.write()
+
+class ZpatecniSvetlo(ZadniSvetlo):
+    def __init__(self, poradi_led, neopixel):
+        super().__init__(poradi_led, neopixel)
+
+    def zapni_zpatecni():
+        self.nastav_barvu(poradi_led, (60, 60, 60))
+        self.np.write()
+
+class SvetelnyModul:
+
+    def __init__(self):
+        neopixel = NeoPixel(pin0, 8)
+
+        self.predni_svetla = []
+        self.predni_svetla.append(PredniSvetlo(0, neopixel))
+        self.predni_svetla.append(PredniSvetlo(3, neopixel))
+
+        self.zadni_svetlo = ZadniSvetlo(6, neopixel)
+        self.zpatecni_svetlo = ZpatecniSvetlo(5, neopixel)
+
+        self.blinkry = []
+        self.blinkry.append(Blinkr(1, neopixel))
+        self.blinkry.append(Blinkr(2, neopixel))
+        self.blinkry.append(Blinkr(4, neopixel))
+        self.blinkry.append(Blinkr(7, neopixel))
+
+    def zapni_obrysova(self):
+        for svetlo in self.predni_svetla:
+            svetlo.zapni()
+
+        self.zadni_svetlo.zapni()
+        self.zpatecni_svetlo.zapni()
+
+    def vypni_obrysova(self):
+        for svetlo in self.predni_svetla:
+            svetlo.vypni()
+
+        self.zadni_svetlo.vypni()
+        self.zpatecni_svetlo.vypni()
+
+    def zapni_zpatecni(self):
+        self.zpatecni_svetlo.zapni_zpatecni()
+
+    def vypni_zpatecni(self):
+        self.zadni_svetlo.vypni()
+
+    def zapni_brzdova(self):
+        self.zadni_svetlo.zapni_brzdove()
+        self.zpatecni_svetlo.zapni_brzdove()
+
+    def vypni_brzdova(self):
+        self.zadni_svetlo.vypni()
+        self.zpatecni_svetlo.vypni()
+
+    def blinkry_blikej(self, smer):
+
+        if smer == Konstanty.LEVY:
+            self.blinkry[0].blikej()
+            self.blinkry[2].blikej()
+        elif smer == Konstanty.PRAVY:
+            self.blinkry[1].blikej()
+            self.blinkry[3].blikej()
+        elif smer == Konstanty.VSE:
+            self.blinkry[0].blikej()
+            self.blinkry[2].blikej()
+            self.blinkry[1].blikej()
+            self.blinkry[3].blikej()
+
+    def vypni_blinkry(self):
+        self.blinkr[0].vypni()
+        self.blinkr[2].vypni()
+        self.blinkr[1].vypni()
+        self.blinkr[3].vypni()
+
+
