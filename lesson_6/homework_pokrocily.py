33df7c6b32d2e96e36fd65bf6be20ca581cc8f95
Vojtech Boburka <vojtech@boburka.cz>
2024-10-06 12:37:04 +0200

    Revert "Update homework_pokrocily.py"
    This reverts commit 3bf002494418e6878ae1798a087ed191988ab7fd.

--- lesson_6/homework_pokrocily.py
+++ lesson_6/homework_pokrocily.py
@@ -7,53 +7,22 @@ from microbit import sleep
 # tzn dopredna_rychlost NENÍ v m/s, ale v nějakých nedefinovaných jednotkách…
 # tento problém odstraníme příští hodinu
 
-#funkce pro limit nastaveni rychlosti 0-255
-def clamp(n):
-    return max(min(255, n), 0)
+def jed(dopredna_rychlost, rotace):
+    # TODO
+    d = 0  # tuto hodnotu si musite zmerit na robotovi, zadejte to v m
+    v_l = 0  # TODO vypocitejte dle prednasky 6
+    v_r = 0  # TODO vypocitejte dle prednasky 6
 
-def jed(dopredna_rychlost:float, rotace:float):
-    param_motor_prava_dozadu = b'\x02'
-    param_motor_prava_dopredu = b'\x03'
-    param_motor_leva_dozadu = b'\x04'
-    param_motor_leva_dopredu = b'\x05'
-    
-    d = 0.15 
-    v_l = int(dopredna_rychlost - d * rotace)
-    v_r = int(dopredna_rychlost + d * rotace)
-    print(v_l, " ", v_r)
+    # !!!! POZOR !!!!
+    # nez zadate v_l a v_r motorum, ujistete se,
+    # ze hodnota je mezi 0-255
 
      # TODO nastavte rychlost a smer motoru podle hodnot v_l a v_r
-    i2c.init(freq=400000)
-    #kontrola rychlosti a smeru praveho kola
-    if v_r > 0:
-        i2c.write(0x70, param_motor_prava_dopredu + bytes([clamp(v_r)]))
-    elif v_r < 0:
-        i2c.write(0x70, param_motor_prava_dozadu + bytes([clamp(abs(v_r))]))
-    elif v_r == 0:
-        i2c.write(0x70, param_motor_prava_dozadu + bytes([0]))
-        i2c.write(0x70, param_motor_prava_dopredu + bytes([0]))
-  
-    #kontrola rychlosti a smeru leveho kola      
-    if v_l > 0:
-        i2c.write(0x70, param_motor_leva_dopredu + bytes([clamp(v_l)]))
-    elif v_l < 0:
-        i2c.write(0x70, param_motor_leva_dozadu + bytes([clamp(abs(v_l))]))
-    elif v_l == 0:
-        i2c.write(0x70, param_motor_leva_dozadu + bytes([0]))
-        i2c.write(0x70, param_motor_leva_dopredu + bytes([0])) 
-        
-#hlavni fce
-def main():
-    i2c.write(0x70, b"\x00\x01")
-    i2c.write(0x70, b"\xE8\xAA")
-    jed(135, 0)
-    sleep(1000) 
-    jed(0, 0)
-    sleep(1000) 
-    jed(0, 1350)
-    sleep(1000) 
-    jed(0, 0)
-    
-#main
+
 if __name__ == "__main__":
-    main()
+
+    # TODO napiste kod, aby robot jel rychlosti "135" 1s vpred
+    # pak zastavil na 1s
+    # pak rotoval okolo sve osy 1s s rychlosti "1350"  po dobu 1s
+    # a pak zastavil a kod se vypnul
+
