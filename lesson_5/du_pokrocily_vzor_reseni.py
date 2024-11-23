fe06d750d6da1a7b779d4f5369b829ff0938e63e
Lenka Mudrich <lenka.mudrich@gmail.com>
2024-11-23 15:02:20 +0000

    Prejmovani souboru a pridane komentare

--- lesson_5/du_pokrocily_vzor_reseni.py
+++ lesson_5/du_pokrocily_vzor_reseni.py
@@ -1,16 +1,28 @@
 from microbit import i2c, sleep
 
 def nastav_PWM_kanaly(kanal_on, kanal_off, rychlost):
+    # je nesmirne dulezite vzdy mit zapnuty jen jeden kanal,
+    # tedy tato funkce zarucuje, ze se druhy kanal vypne
     i2c.write(0x70, kanal_off + bytes([0]))
     i2c.write(0x70, kanal_on + bytes([rychlost]))
     return 0
 
 def jed(motor, smer, rychlost):
-    je_vse_ok = -2
+    '''
+       Tato funkce uvede dany motor do pohybu,
+       zadanou rychlosti a v pozadovanem smeru.
+       Vraci chybove hodnoty:
+        0: vse je v poradku
+       -1: program neprobehl spravne
+       -2: zadane nepodporovane jmeno motoru
+       -3: pozadovana rychlost je mimo mozny rozsah
+       -4: zadany nepodporovany smer
+    '''
+    je_vse_ok = -1
 
     rychlost = int(rychlost)
     if rychlost < 0 or rychlost > 255:
-        je_vse_ok = -1
+        je_vse_ok = -3
         return
 
     if motor == "levy":
@@ -19,16 +31,16 @@ def jed(motor, smer, rychlost):
         elif smer == "dozadu":
             je_vse_ok = nastav_PWM_kanaly(b"\x04", b"\x05", rychlost)
         else:
-            je_vse_ok = -1
+            je_vse_ok = -4
     elif motor == "pravy":
         if smer == "dopredu":
             je_vse_ok = nastav_PWM_kanaly(b"\x03", b"\x02", rychlost)
         elif smer == "dozadu":
             je_vse_ok = nastav_PWM_kanaly(b"\x02", b"\x03", rychlost)
         else:
-            je_vse_ok = -1
+            je_vse_ok = -4
     else:
-        je_vse_ok = -1
+        je_vse_ok = -2
 
     return je_vse_ok
 
